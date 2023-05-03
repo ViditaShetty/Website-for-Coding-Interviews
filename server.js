408ae89f2bdbcdd5090ae414dcca79744e3e6b7c
ViditaShetty <viditashetty9@gmail.com>
2023-05-03 16:21:30 +0530

    chnageed url

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+  res.redirect(`/api/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
+app.get('/api/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
